6c3de29df5ea81e7aee99abfb5f20d05f143ef53
Taneros <fricentaurus@GMAIL.COM>
2022-07-16 12:19:49 +0300

    feat: setup json-server

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from '../../api/axios'
+
+const URL = '/table'
 
 export const Main = () => {
+  const getTableData = async () => {
+    try {
+      const res = await axios.get(URL)
+      console.log(`res`, res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getTableData()
+    // setTableData([...data])
+  }, [])
+
   return <div>Main</div>
 }
